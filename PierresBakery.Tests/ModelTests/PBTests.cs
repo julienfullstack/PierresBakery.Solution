5c6ee7de035e3bc8b30b94b1687f204ccc7e88c6
Julien L <julienlenaz96@gmail.com>
2023-09-09 23:02:28 -0400

    Passed second test

--- PierresBakery.Tests/ModelTests/PBTests.cs
+++ PierresBakery.Tests/ModelTests/PBTests.cs
@@ -12,5 +12,11 @@ namespace PierresBakery.Tests
             Bread newBread = new Bread(1);
             Assert.AreEqual(typeof(Bread), newBread.GetType());
         }
+        [TestMethod]
+        public void PastryConstructer_CreatesInstanceOfPastry_Pastry()
+        {
+          Pastry newPastry = new Pastry(1);
+          Assert.AreEqual(typeof(Pastry), newPastry.GetType());
+        }
     }
 }
