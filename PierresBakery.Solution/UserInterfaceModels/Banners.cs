5e699a8acfa4ea4f45a3f3afa52975c505561b07
Julien L <julienlenaz96@gmail.com>
2023-09-12 21:21:14 -0400

    Renamed repo, updated readme

---
+++ PierresBakery.Solution/UserInterfaceModels/Banners.cs
@@ -0,0 +1,20 @@
+namespace PierresBakery.UserInterfaceModels
+{
+  class WelcomeBanner
+  {
+    public static string Welcome { get; } = @"            /                  
+ _ . _ _ _ _  _ |_  _ |  _ _   
+|_)|(-| | (- _) |_)(_||((-| \/ 
+|                           /  ";
+  }                                                                                        
+
+  class GoodbyeBanner
+  {
+  
+  }
+
+  class SubMenuBanner
+  {
+
+  }
+}
